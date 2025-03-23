f8effe6677248254642ab5efed01ffa53af3168b
rabf1969 <rabf44@gmail.com>
2025-03-22 21:28:54 -0300

    mais um arquivo de configuração do git

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,51 @@
-function App() {
-  const myText = "Hello World";
-  
+import {
+  Title,
+  Container,
+  TopBackground,
+  Form,
+  ContainerInputs,
+  Input,
+  Button,
+  ImputLabel,
+} from "./styles";
+import UsersImage from "./assets/users.png";
+function Home() {
   return (
-    
-      <div>
-        <h1>{myText}</h1>
-      </div>
-   
-  )
+    <Container>
+      <TopBackground>
+        <img src={UsersImage} alt="Imagem-de-usuários" />
+      </TopBackground>
+
+      <Form>
+        <Title>Cadastrar Usuário</Title>
+
+        <ContainerInputs>
+          <div>
+            <ImputLabel>
+              Nome<span> *</span>
+            </ImputLabel>
+            <Input type="text" placeholder="Nome do Usuário" />
+          </div>
+
+          <div>
+            <ImputLabel>
+              Idade<span> *</span>
+            </ImputLabel>
+            <Input type="number" placeholder="Idade do Usuário" />
+          </div>
+        </ContainerInputs>
+
+        <div style={{ width: "100%" }}>
+          <ImputLabel>
+            E-mail<span> *</span>
+          </ImputLabel>
+          <Input type="email" placeholder="Email do Usuário" />
+        </div>
+
+        <Button>Cadastrar Usuário</Button>
+      </Form>
+    </Container>
+  );
 }
 
-export default App;
+export default Home;
