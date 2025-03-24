10e309acb169f15668c08c591a500c380553308b
RABF1969 <rabf44@gmail.com>
2025-03-24 18:13:44 -0300

    alteraçao no componente button

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -68,23 +68,6 @@ span {
 }
 `;
 
-export const Button = styled.button`
-border: none;
-background: linear-gradient(180deg, #FE7E5D, #FF6378 100%);
-font-size: 16px;
-padding: 16px 32px;
-width: fit-content;
-border-radius: 30px;
-cursor: pointer;
 
-&:hover {
-  opacity: 0.8;
-}
-
-&:active {
-  opacity: 0.5;
-}   
-
-`;
 
 
